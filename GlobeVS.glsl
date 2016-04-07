415781eed969221c9a7c7cdd6d3421fdac85eedf
RallyTronics <john@rallytronics.com>
2016-04-07 11:45:27 -0600

    modified to be similar to bgfx vs_cubes.sc

--- GlobeVS.glsl
+++ GlobeVS.glsl
@@ -1,4 +1,6 @@
-#version 330
+$input a_position
+$output v_position
+
 //
 // (C) Copyright 2010 Patrick Cozzi and Deron Ohlarik
 //
@@ -6,13 +8,17 @@
 // See License.txt or http://www.opensource.org/licenses/mit-license.php.
 //
 
-layout(location = og_positionVertexLocation) in vec4 position;
-out vec3 worldPosition;
+//layout(location = og_positionVertexLocation) in vec4 position;
+//out vec3 worldPosition;
 
-uniform mat4 og_modelViewPerspectiveMatrix;
+//uniform mat4 og_modelViewPerspectiveMatrix;
+
+#include "../common/common.sh"
 
 void main()                     
 {
-    gl_Position = og_modelViewPerspectiveMatrix * position; 
-    worldPosition = position.xyz;
+    //gl_Position = og_modelViewPerspectiveMatrix * position; 
+	gl_Position = mul(u_modelViewProj, vec4(a_position, 1.0) );
+    //worldPosition = position.xyz;
+    v_position = a_position;
 }
